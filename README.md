ad785d00857eae07cfe369e35ab96dcb1db81ec7
Sudesh Manjrekar <sudath6258@gmail.com>
2022-01-24 15:04:55 +0530

    Modified Readme and added UI files

--- README.md
+++ README.md
@@ -1,2 +1,167 @@
-# Tic_Tac_Toe
-A Tic Tac Toe game made using Pygame and Tkinter
+# ❌ Tic Tac Toe ⭕
+A Tic Tac Toe game made using Pygame Tkinter and Numpy. The game functions are all written in Python and the matrix for the game is created using Numpy and the GUI for the Game is developed using Pygame. The dialog boxes indicating the player who has won or if it was a draw are all developed using Tkinter.
+
+## Initializing the game 👾
+The game initializes by creating a matrix using Numpy as `np.zeroes()` which takes 2 arguments: row and column, and then creates a 3x3 matrix containing all elements as 0s.
+```Python
+def create_board():
+    board = np.zeros((row_count, column_count))
+    return board
+```
+And then it creates a board using Pygame
+```Python
+def draw_board(board):
+    for c in range(column_count):
+        for r in range(row_count):
+            pygame.draw.aaline(screen, white, (0, 100), (300, 100))
+            pygame.draw.aaline(screen, white, (0, 200), (300, 200))
+            pygame.draw.aaline(screen, white, (0, 300), (300, 300))
+            pygame.draw.aaline(screen, white, (0, 400), (300, 400))
+            pygame.draw.aaline(screen, white, (0, 100), (0, 400))
+            pygame.draw.aaline(screen, white, (100, 100), (100, 400))
+            pygame.draw.aaline(screen, white, (200, 100), (200, 400))
+            pygame.draw.aaline(screen, white, (299, 100), (299, 400))
+    pygame.display.update()
+```
+
+## Checking for a valid move ✅
+The function `is_valid_move()` checks whether a move is valid before allowing it. 
+```Python
+def is_valid_move(board, row, column, piece):
+    if board[row][column] == piece:
+        return True
+```
+
+## Deploying Pieces on Board and into a Matrix 🤔
+So how does it all work?? The function drop_piece takes in 4 arguments namely: board, piece, row & column. The piece is a numerical value of either '1' or '2' corresponding to the Player number. The piece is entered into the game matrix. The row and column are then decided and then the piece either ❌ or ⭕ is displayed on the Pygame GUI using `screen.blit()` function at a particular row and column intersection.
+```Python
+def drop_piece(board, piece, row, column):
+    if board[row][column] == 0:
+        board[row][column] = piece
+
+    row_w = (column*100) + 35
+    column_h = (row*100) + 125
+
+    if board[row][column] == 1:
+        font = pygame.font.SysFont("monospace", 50)
+        img = font.render("X", 1, Red)
+        screen.blit(img, (int(row_w), int(column_h)))
+    elif board[row][column] == 2:
+        font = pygame.font.SysFont("monospace", 50)
+        img = font.render("O", 1, Yellow)
+        screen.blit(img, (int(row_w), int(column_h)))
+
+```
+
+## Checking for a Draw! 😑
+We really hate it when there's a draw but we have to include it. So when no player is winning, the `is_draw()` function checks if there are any availabe spaces i.e. it checks if there are any zeroes in the matrix.
+```Python
+def is_draw(board, row, col):
+    if board.min()!=0:
+        return True
+```
+
+## Checking for a Winner ✨
+The winner player is determined by checking in 4 different ways: row wise, column wise, and 2 ways diagonally.
+```Python
+def winning_move(board, piece):
+    #Loop for Checking winning condition horizontally
+    for row in range(row_count):
+        if (board[row][0] == piece) and (board[row][1] == piece) and (board[row][2] == piece):
+            return True
+
+    #Loop for Checking winning condition vertically
+    for column in range(column_count):
+        if (board[0][column] == piece) and (board[1][column] == piece) and (board[2][column] == piece):
+            return True
+        
+    #Loop for Checking winning condition in positive slope
+    if (board[2][0] == piece) and (board[1][1] == piece) and (board[0][2] == piece):
+            return True
+
+    #Loop for Checking winning condition in negative slope
+    if (board[0][0] == piece) and (board[1][1] == piece) and (board[2][2] == piece):
+            return True
+```
+
+## The Game Function 👑
+The game function is the real hero 🐱‍🏍that brings it all together it takes the parameter turn which determines which one of the player gets to go first. It declares all the necessary functions like closing window when X is clicked. Then according to whose turn it is, whenever the mouse is in motion, the player whose turn it is, is refected on the top of the UI and then the display is updated. Then the row and column is detected by the position of the mouse pointer. Then accoring to which player's turn it is, the program sequentially drops a piece, checks for a winning move, checks if there are valid moves an then changes the turn at the end it checks if it is a draw and then it updates the display.
+```Python
+def game(turn):
+    while not gameover:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+            pos = pygame.mouse.get_pos()
+            
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, Black, (0, 0, 300, 100))
+                if turn == 0:
+                    label = myfont.render("Player 1's turn!!", 1, Red)
+                    screen.blit(label, (5, 10))
+                else:
+                    # if ply2=="":
+                    label = myfont.render("Player 2's turn!!", 1, Red)
+                    screen.blit(label, (5, 10))
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                posx = event.pos[0]
+                posy = event.pos[1]
+
+                if posy>100 and posy<400:                        
+                    posx = event.pos[0]
+                    posy = event.pos[1]
+                    col = int(math.floor(posx/100))
+                    row = int(math.floor(posy/100)) - 1
+                
+                    if turn == 0:
+                        drop_piece(board, 1, row, col)
+                        print(board)
+                        
+                        if winning_move(board, 1):
+                            pygame.draw.rect(screen, Black, (0, 0, 300, 100))
+                            label = myfont.render("Player 1 wins!!", 1, Red)
+                            screen.blit(label, (5, 10))
+                            draw_board(board)
+                            ask_quit_or_playagain(1, withdraw)
+
+                        if is_valid_move(board, row, col, 1):
+                            turn+=1
+                            turn%=2
+
+                    elif turn == 1:
+                        drop_piece(board, 2, row, col)
+                        print(board)
+
+                        if winning_move(board, 2):
+                            pygame.draw.rect(screen, Black, (0, 0, 300, 100))
+                            label = myfont.render("Player 2 wins!!", 1, Red)
+                            screen.blit(label, (5, 10))
+                            draw_board(board)
+                            ask_quit_or_playagain(2, withdraw)
+
+                        if is_valid_move(board, row, col, 2):
+                            turn+=1
+                            turn%=2
+                    draw_board(board)
+
+                    if is_draw(board, row, col):
+                        pygame.draw.rect(screen, Black, (0, 0, 300, 100))
+                        label = myfont.render("It's a draw!!", 1, Red)
+                        screen.blit(label, (5, 10))
+                        ask_quit_or_playagain(0, withdraw)
+            pygame.display.update()
+```
+
+## Snaps of Game in Action 🎮
+<p align=center>
+  <img src="Resources/UI.svg">
+  <p align=center>Game UI</p>
+</p>
+
+<p align=center>
+  <img src="Resources/Dialog.svg">
+  <p align=center>Game winner Prompt</p>
+</p>
--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -58,12 +58,12 @@ def drop_piece(board, piece, row, column):
         screen.blit(img, (int(row_w), int(column_h)))
 
 def winning_move(board, piece):
-    #Loop for Checking winning condition in row
+    #Loop for Checking winning condition horizontally
     for row in range(row_count):
         if (board[row][0] == piece) and (board[row][1] == piece) and (board[row][2] == piece):
             return True
 
-    #Loop for Checking winning condition in column
+    #Loop for Checking winning condition vertically
     for column in range(column_count):
         if (board[0][column] == piece) and (board[1][column] == piece) and (board[2][column] == piece):
             return True
@@ -80,101 +80,10 @@ def is_draw(board, row, col):
     if board.min()!=0:
         return True
 
-# class button():
-#     def __init__(self, color, x,y,width,height, text=''):
-#         self.color = color
-#         self.x = x
-#         self.y = y
-#         self.width = width
-#         self.height = height
-#         self.text = text
-
-#     def draw(self,win,outline=None):
-#         #Call this method to draw the button on the screen
-#         if outline:
-#             pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
-            
-#         pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
-        
-#         if self.text != '':
-#             font = pygame.font.SysFont('monospace', 15)
-#             text = font.render(self.text, 1, (0,0,0))
-#             win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
-
-#     def isOver(self, pos):
-#         #Pos is the mouse position or a tuple of (x,y) coordinates
-#         if pos[0] > self.x and pos[0] < self.x + self.width:
-#             if pos[1] > self.y and pos[1] < self.y + self.height:
-#                 return True
-            
-#         return False
-
-# def player_name():
-#     if withdraw==1:
-#         # for widgets in root.winfo_children():
-#         #     widgets.destroy()
-#         root.title("Player Names")
-#         p1_label = Label(root, text="Player 1")
-#         p1_label.grid(row=0, column=0, padx=20, pady=20)
-#         p2_label = Label(root, text="Player 2")
-#         p2_label.grid(row=1, column=0)
-
-#         p1_name = Entry(root, width=30)
-#         p1_name.grid(row=0, column=1, padx=20, pady=20)
-#         p1_name.focus_set()
-#         p2_name = Entry(root, width=30)
-#         p2_name.grid(row=1, column=1)
-
-#         save = Button(root,text = "Save Changes", command=lambda: save_names(p1_name.get(), p2_name.get()))
-#         save.grid(row = 2, column = 1, padx=20, pady=20)
-#         back = Button(root,text = "Back", command=lambda: save_names(p1_name.get(), p2_name.get()))
-#         back.grid(row = 2, column = 2, pady=20)
-#         root.deiconify()
-#         root.mainloop()
-
-#     else:
-#         # for widgets in root.winfo_children():
-#         #     widgets.destroy()
-#         root.title("Player Names")
-
-#         p1_label = Label(root, text="Player 1")
-#         p1_label.grid(row=0, column=0, padx=20, pady=20)
-
-#         p2_label = Label(root, text="Player 2")
-#         p2_label.grid(row=1, column=0)
-
-#         p1_name = Entry(root, width=30)
-#         p1_name.grid(row=0, column=1, padx=20, pady=20)
-#         p1_name.focus_set()
-
-#         p2_name = Entry(root, width=30)
-#         p2_name.grid(row=1, column=1)
-
-#         save = Button(root,text = "Save Changes", command=lambda: save_names(p1_name.get(), p2_name.get()))
-#         save.grid(row = 2, column = 1, padx=20, pady=20)
-#         back = Button(root,text = "Back", command=lambda: save_names(p1_name.get(), p2_name.get()))
-#         back.grid(row = 2, column = 2, pady=20)
-#         root.deiconify()
-#         root.mainloop()
-
-# def save_names(p1, p2):
-#     global ply1, ply2
-#     ply1 = p1
-#     ply2 = p2
-#     root.withdraw()
-#     game(random.randint(0,1))
-#     withdraw = True
-#     return
-
 def ask_quit_or_playagain(piece, withdraw):
     if withdraw == 1:
         if piece == 1:
-            # for widgets in root.winfo_children():
-            #     widgets.destroy()
-            # if ply1=="":
             my_label = Label(root, text="Player 1 wins!!! Do you want to play again?")
-            # else:
-            #     my_label = Label(root, text=str(ply1) + "wins!!! Do you want to play again?")
             my_label.grid(row=0, column=0, pady=30, padx=10, sticky="wens")
             button_1 = Button(root, text="Quit", command=quit_)
             button_1.grid(row=1, column=1, padx=10, pady=10)
@@ -184,12 +93,7 @@ def ask_quit_or_playagain(piece, withdraw):
             root.mainloop()
 
         elif piece == 2:
-            # for widgets in root.winfo_children():
-            #     widgets.destroy()
-            # if ply2=="":
             my_label = Label(root, text="Player 2 wins!!! Do you want to play again?")
-            # else:
-            #     my_label = Label(root, text=str(ply2) + "wins!!! Do you want to play again?")
             my_label.grid(row=0, column=0, pady=30, padx=10, sticky="wens")
             button_1 = Button(root, text="Quit", command=quit_)
             button_1.grid(row=1, column=1, padx=10, pady=10)
@@ -199,8 +103,6 @@ def ask_quit_or_playagain(piece, withdraw):
             root.mainloop()
 
         else:
-            # for widgets in root.winfo_children():
-            #     widgets.destroy()
             my_label = Label(root, text="It's a draw!!! Do you want to play again?")
             my_label.grid(row=0, column=0, pady=30, padx=10, sticky="wens")
             button_1 = Button(root, text="Quit", command=quit_)
@@ -212,12 +114,7 @@ def ask_quit_or_playagain(piece, withdraw):
     else:
         
         if piece == 1:
-            # for widgets in root.winfo_children():
-            #     widgets.destroy()
-            # if ply1=="":
             my_label = Label(root, text="Player 1 wins!!! Do you want to play again?")
-            # else:
-            #     my_label = Label(root, text=str(ply1) + "wins!!! Do you want to play again?")
             my_label.grid(row=0, column=0, pady=30, padx=10, sticky="wens")
             button_1 = Button(root, text="Quit", command=quit_)
             button_1.grid(row=1, column=1, padx=10, pady=10)
@@ -226,12 +123,7 @@ def ask_quit_or_playagain(piece, withdraw):
             root.mainloop()
 
         elif piece == 2:
-            # for widgets in root.winfo_children():
-            #     widgets.destroy()
-            # if ply2=="":
             my_label = Label(root, text="Player 2 wins!!! Do you want to play again?")
-            # else:
-                # my_label = Label(root, text=str(ply2) + "wins!!! Do you want to play again?")
             my_label.grid(row=0, column=0, pady=30, padx=10, sticky="wens")
             button_1 = Button(root, text="Quit", command=quit_)
             button_1.grid(row=1, column=1, padx=10, pady=10)
@@ -240,8 +132,6 @@ def ask_quit_or_playagain(piece, withdraw):
             root.mainloop()
 
         else:
-            # for widgets in root.winfo_children():
-            #     widgets.destroy()
             my_label = Label(root, text="It's a draw!!! Do you want to play again?")
             my_label.grid(row=0, column=0, pady=30, padx=10, sticky="wens")
             button_1 = Button(root, text="Quit", command=quit_)
@@ -277,12 +167,6 @@ def play_again():
     turn = random.randint(0,1)
     myfont = pygame.font.SysFont("monospace", 25)
 
-    # change_name = button((0,255,0), 15, 415, 125, 25, 'Player names')
-    # change_name.draw(screen, white)
-
-    # change_theme = button((0,255,0), 155, 415, 125, 25, 'Change Theme')
-    # change_theme.draw(screen, white)
-
     while not gameover:
 
         for event in pygame.event.get():
@@ -291,41 +175,20 @@ def play_again():
             pos = pygame.mouse.get_pos()
             
             if event.type == pygame.MOUSEMOTION:
-                # if change_name.isOver(pos):
-                #     change_name.color = (255, 0, 0)
-                #     change_name.draw(screen)
-                # else: 
-                #     change_name.color = (0, 255, 0)
-                #     change_name.draw(screen)
-                # if change_theme.isOver(pos):
-                #     change_theme.color = (255, 0, 0)
-                #     change_theme.draw(screen)
-                # else: 
-                #     change_theme.color = (0, 255, 0)
-                #     change_theme.draw(screen)
                 pygame.draw.rect(screen, Black, (0, 0, 300, 100))
                 if turn == 0:
-                    # if ply1=="":
                     label = myfont.render("Player 1's turn!!", 1, Red)
-                    # else:
-                    #     label = myfont.render(ply1 + "'s turn!!", 1, Red)
                     screen.blit(label, (5, 10))
                                 
                 else:
-                    # if ply2=="":
                     label = myfont.render("Player 2's turn!!", 1, Red)
-                    # else:
-                    #     label = myfont.render(ply2 + "'s turn!!", 1, Red)
                     screen.blit(label, (5, 10))
             pygame.display.update()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 posx = event.pos[0]
                 posy = event.pos[1]
-                # if change_name.isOver(pos):
-                #         player_name()
-                # if change_theme.isOver(pos):
-                #         print("Button was clicked")
+
                 if posy>100 and posy<400:
                     posx = event.pos[0]
                     posy = event.pos[1]
@@ -335,15 +198,14 @@ def play_again():
                     if turn == 0:
                         drop_piece(board, 1, row, col)
                         print(board)
+
                         if winning_move(board, 1):
                             pygame.draw.rect(screen, Black, (0, 0, 300, 100))
-                            # if ply2=="":
                             label = myfont.render("Player 1 wins!!", 1, Red)
-                            # else:
-                            #     label = myfont.render(ply1 + " wins!!", 1, Red)
                             screen.blit(label, (5, 10))
                             draw_board(board)
                             ask_quit_or_playagain(1, withdraw)
+
                         if is_valid_move(board, row, col, 1):
                             turn+=1
                             turn%=2
@@ -351,19 +213,19 @@ def play_again():
                     elif turn == 1:
                         drop_piece(board, 2, row, col)
                         print(board)
+
                         if winning_move(board, 2):
                             pygame.draw.rect(screen, Black, (0, 0, 300, 100))
-                            # if ply2=="":
                             label = myfont.render("Player 2 wins!!", 1, Red)
-                            # else:
-                            #     label = myfont.render(ply2 + " wins!!", 1, Red)
                             screen.blit(label, (5, 10))
                             draw_board(board)
                             ask_quit_or_playagain(2, withdraw)
+
                         if is_valid_move(board, row, col, 2):
                             turn+=1
                             turn%=2
                     draw_board(board)
+
                     if is_draw(board, row, col):
                         pygame.draw.rect(screen, Black, (0, 0, 300, 100))
                         label = myfont.render("It's a draw!!", 1, Red)
@@ -377,50 +239,28 @@ def is_valid_move(board, row, column, piece):
 
 
 def game(turn):
-        
     while not gameover:
-
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 sys.exit()
             pos = pygame.mouse.get_pos()
             
             if event.type == pygame.MOUSEMOTION:
-                # if change_name.isOver(pos):
-                #     change_name.color = (255, 0, 0)
-                #     change_name.draw(screen)
-                # else: 
-                #     change_name.color = (0, 255, 0)
-                #     change_name.draw(screen)
-                # if change_theme.isOver(pos):
-                #     change_theme.color = (255, 0, 0)
-                #     change_theme.draw(screen)
-                # else: 
-                #     change_theme.color = (0, 255, 0)
-                #     change_theme.draw(screen)
                 pygame.draw.rect(screen, Black, (0, 0, 300, 100))
+
                 if turn == 0:
-                    # if ply1=="":
                     label = myfont.render("Player 1's turn!!", 1, Red)
-                    # else:
-                    #     label = myfont.render(ply1 + "'s turn!!", 1, Red)
                     screen.blit(label, (5, 10))
-                                
                 else:
-                    # if ply2=="":
                     label = myfont.render("Player 2's turn!!", 1, Red)
-                    # else:
-                    #     label = myfont.render(ply2 + "'s turn!!", 1, Red)
                     screen.blit(label, (5, 10))
             pygame.display.update()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 posx = event.pos[0]
                 posy = event.pos[1]
-                # if change_name.isOver(pos):
-                #         player_name()
-                # if change_theme.isOver(pos):
-                #         print("Button was clicked")
+
                 if posy>100 and posy<400:                        
                     posx = event.pos[0]
                     posy = event.pos[1]
@@ -430,35 +270,34 @@ def game(turn):
                     if turn == 0:
                         drop_piece(board, 1, row, col)
                         print(board)
+
                         if winning_move(board, 1):
                             pygame.draw.rect(screen, Black, (0, 0, 300, 100))
-                            # if ply1=="":
                             label = myfont.render("Player 1 wins!!", 1, Red)
-                            # else:
-                            #     label = myfont.render(ply1 + " wins!!", 1, Red)
                             screen.blit(label, (5, 10))
                             draw_board(board)
                             ask_quit_or_playagain(1, withdraw)
+
                         if is_valid_move(board, row, col, 1):
                             turn+=1
                             turn%=2
-                        
+
                     elif turn == 1:
                         drop_piece(board, 2, row, col)
                         print(board)
+
                         if winning_move(board, 2):
                             pygame.draw.rect(screen, Black, (0, 0, 300, 100))
-                            # if ply2=="":
                             label = myfont.render("Player 2 wins!!", 1, Red)
-                            # else:
-                            #     label = myfont.render(ply2 + " wins!!", 1, Red)
                             screen.blit(label, (5, 10))
                             draw_board(board)
                             ask_quit_or_playagain(2, withdraw)
+
                         if is_valid_move(board, row, col, 2):
                             turn+=1
                             turn%=2
                     draw_board(board)
+                    
                     if is_draw(board, row, col):
                         pygame.draw.rect(screen, Black, (0, 0, 300, 100))
                         label = myfont.render("It's a draw!!", 1, Red)
@@ -482,11 +321,5 @@ gameover = False
 turn = 0
 myfont = pygame.font.SysFont("monospace", 25)
 pygame.display.update()
-
-# change_name = button((0, 255, 0), 15, 415, 125, 25, 'Player names')
-# change_name.draw(screen, white)
-
-# change_theme = button((0, 255, 0), 155, 415, 125, 25, 'Change theme')
-# change_theme.draw(screen, white)
 
 game(random.randint(0,1))
